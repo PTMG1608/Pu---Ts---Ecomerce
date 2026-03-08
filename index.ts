8a7148bf4cd42eb5034860738a3429a1e27b9f3d
Pedro Giffoni <pedrotonimelo@gmail.com>
2026-03-08 17:18:05 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,8 +2,10 @@ import { Product } from "./model/product_model"; // Importando a classe Product 
 import { Category } from "./model/category_model"; // Importando a classe Category do arquivo category_model.ts
 import { Cart } from "./model/Cart_models"; //  Importando a classe Cart do arquivo Cart_models.ts
 import { User, UserRole } from "./model/user_model"; // Importando a classe User e o enum UserRole do arquivo user_model.ts
+
 // Criando uma categoria
 const category = new Category(1, "Instrument");
+
 // Criando um produto e associando à categoria
 const product = new Product(
   1,
@@ -14,30 +16,30 @@ const product = new Product(
   0.2,
 );
 // Criando um carrinho e adicionando produtos
-const cart = new Cart();
-cart.addItem(product, 2);
-cart.addItem(product, 1);
-cart.addItem(product, 3);
+const cart = new Cart(); // Criando um carrinho
+cart.addItem(product, 2); // Adicionando produtos
+cart.addItem(product, 1); // Adicionando produtos
+cart.addItem(product, 3); // Adicionando produtos
 
 // Criando usuários
-const admin = new User(1, "pedro", "pedrotonimelo@gmail.com", UserRole.ADMIN);
-const customer = new User(2, "joao", "joao@email.com", UserRole.CUSTOMER);
+const admin = new User(1, "pedro", "pedrotonimelo@gmail.com", UserRole.ADMIN); // Exibindo os usuários criados, incluindo suas informações como id, username, email e role
+const customer = new User(2, "joao", "joao@email.com", UserRole.CUSTOMER); // Exibindo os usuários criados, incluindo suas informações como id, username, email e role
 
-console.log(admin);
-console.log(customer);
+console.log(admin); // Exibindo os usuários criados, incluindo suas informações como id, username, email e role
+console.log(customer); // Exibindo os usuários criados, incluindo suas informações como id, username, email e role
 
-console.log("produto nome: ", product.name);
-console.log("categoria nome: ", category.name);
-console.log("carrinho: ", cart.listProduct);
-console.log("preço com desconto: ", product.PriceWithDiscountApplied());
+console.log("produto nome: ", product.name); // Exibindo o nome do produto
+console.log("categoria nome: ", category.name); // Exibindo o nome do produto e o nome da categoria associada ao produto
+console.log("carrinho: ", cart.listProduct); // Exibindo o conteúdo do carrinho, que inclui os produtos e suas quantidades
+console.log("preço com desconto: ", product.PriceWithDiscountApplied()); // Exibindo o preço do produto com o desconto aplicado
 console.log(
   "Quantidade de itens no carrinho: ",
   cart.getTotalItems(),
   "unidades",
-);
+); // Exibindo a quantidade total de itens no carrinho
 
-console.log("Preço total do carrinho:", cart.getFinalPrice(), "reais");
+console.log("Preço total do carrinho:", cart.getFinalPrice(), "reais"); // Exibindo o preço total do carrinho, aplicando os descontos dos produtos
 console.log(
   "categoria dentro do carrinho:",
-  cart.listProduct[0].product.category,
+  cart.listProduct[0].product.category, // Acessando a categoria do produto dentro do carrinho
 );
